36588b9ea747c04fd5e9a65415b617a991884676
giannip <perkinsgianni@gmail.com>
2026-08-07 14:36:31 -0400

    8/7 | Assignment 4 - Mentor review feedback: 'Hire Date' fix

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -120,7 +120,7 @@ print(f"Mean ages, median salaries:\n {clean_data}")
 
 # convert hire date to datetime
 # "coerce" replaces placeholders with NaT, format="mixed" allows multiple formats
-clean_data['Hire Date'] = pd.to_datetime(clean_data['Hire Date'], errors="coerce")
+clean_data['Hire Date'] = pd.to_datetime(clean_data['Hire Date'], errors="coerce", format="mixed")
 
 # fill NaT with timestamp (pd equivalent of python’s datetime) to enforce datetime type
 # clean_data['Hire Date'] = clean_data['Hire Date'].fillna(pd.Timestamp('2023-01-01'))
